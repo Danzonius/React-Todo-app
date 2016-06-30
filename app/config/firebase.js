4c7213c4629c8ee2432de4688b20bc6e478c7567
Tycho Atsma <tycho1996@gmail.com>
2016-06-30 15:00:17 +0200

    Ignored config file

--- app/config/firebase.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBfIJg8qA1vDp4uy_3Ox_Qh0X4TBborsN0",
-  authDomain: "my-first-project-284fb.firebaseapp.com",
-  databaseURL: "https://my-first-project-284fb.firebaseio.com",
-  storageBucket: "my-first-project-284fb.appspot.com",
-};
-
-export default config;
